df3f4d621e26cbcb4c86f65e1be7dcd9a9a3a002
joeloster <joel.osterdahl16@gmail.com>
2025-12-14 20:09:58 +0100

    update

--- src/Crossword.jsx
+++ src/Crossword.jsx
@@ -6,7 +6,7 @@ import Clues from "./components/Clues";
 
 function Crossword() {
     return (
-        <div className="flex flex-col lg:flex-row items-center justify-center py-4 gap-8 min-h-screen overflow-auto bg-[#FFFFE3]">
+        <div className="flex flex-col lg:flex-row items-center justify-center py-4 gap-8 min-h-screen overflow-auto">
             <div className="flex flex-col gap-2">
                 <div className="flex justify-end">
                     <Show_solution_button />
--- src/components/Clues.jsx
+++ src/components/Clues.jsx
@@ -4,12 +4,10 @@ import { clues } from "../misc/clues";
 
 function Clues() {
     return (
-        <div className="flex flex-col items-center gap-4 w-1/2 lg:w-1/4 border border-black rounded-md">
-            <div className="text-xl font-bold w-full text-center bg-[#f8b22e] p-2 rounded-t-md">Clues</div>
+        <div className="flex flex-col items-center p-4 gap-4 w-full lg:w-1/4">
+            <div className="text-xl font-bold">Clues</div>
             {clues.map((clue, i) => (
-                <div className="p-1" key={i}>
-                    {clue.clue}
-                </div>
+                <div key={i}>{clue.clue}</div>
             ))}
         </div>
     );
